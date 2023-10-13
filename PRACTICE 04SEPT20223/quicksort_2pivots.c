a597a23ce7dcaee3e60d871a02b7d3d703d6c4cc
Charish53 <123405278+Charish53@users.noreply.github.com>
2023-10-14 00:54:38 +0530

    Create quicksort_2pivots.c

---
+++ PRACTICE 04SEPT20223/quicksort_2pivots.c
@@ -0,0 +1,78 @@
+/*Implement quick sort with two pivots. */
+#include <stdio.h>
+
+void dualPivotQuickSort(int arr[], int low, int high);
+
+void partition(int arr[], int low, int high, int *p1, int *p2);
+
+void swap(int *a, int *b);
+void dualPivotQuickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int p1, p2;
+        partition(arr, low, high, &p1, &p2);
+        dualPivotQuickSort(arr, low, p1 - 1);
+        dualPivotQuickSort(arr, p1 + 1, p2 - 1);
+        dualPivotQuickSort(arr, p2 + 1, high);
+    }
+}
+void partition(int arr[], int low, int high, int *p1, int *p2) {
+    if (arr[low] > arr[high]) {
+        swap(&arr[low], &arr[high]);
+    }
+    int pivot1 = arr[low];
+    int pivot2 = arr[high];
+    int i = low + 1;
+    int j = high - 1;
+    int k = low + 1;
+
+    while (k <= j) {
+        if (arr[k] < pivot1) {
+            swap(&arr[k], &arr[i]);
+            i++;
+        } else if (arr[k] >= pivot2) {
+            while (arr[j] > pivot2 && k < j) {
+                j--;
+            }
+            swap(&arr[k], &arr[j]);
+            j--;
+            if (arr[k] < pivot1) {
+                swap(&arr[k], &arr[i]);
+                i++;
+            }
+        }
+        k++;
+    }
+
+    i--;
+    j++;
+
+    swap(&arr[low], &arr[i]);
+    swap(&arr[high], &arr[j]);
+
+    *p1 = i;
+    *p2 = j;
+}
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+    int n,arr[1000];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++)
+    printf("%d ",arr[i]);
+    printf("\n");
+    dualPivotQuickSort(arr, 0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
