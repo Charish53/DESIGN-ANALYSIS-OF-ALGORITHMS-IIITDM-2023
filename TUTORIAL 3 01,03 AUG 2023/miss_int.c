3883afc88cf06701c5bb8d6fb01b99299f920420
Charish53 <123405278+Charish53@users.noreply.github.com>
2023-10-14 00:35:23 +0530

    Create miss_int.c

---
+++ TUTORIAL 3 01,03 AUG 2023/miss_int.c
@@ -0,0 +1,19 @@
+/*Let A be an array of size n filled with distinct integers from the set
+S = {0, 1, . . . , n}. How efficiently can you return the missing integer.
+(exactly one element of S is missing in A).*/
+#include<stdio.h>
+void main()
+{
+    int n,sum=0,miss;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&arr[i]);
+    sum=sum+arr[i];
+    }
+    miss=(n*(n+1))/2-sum;
+    printf("%d",miss);
+}
+
+
